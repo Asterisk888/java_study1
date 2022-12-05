ba83bc88bbddd1351e6442ba938d123702014f90
Asterisk888 <119795023+Asterisk888@users.noreply.github.com>
2022-12-05 23:18:06 +0900

    Add files via upload

---
+++ hello2.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class hello2 {
+		public static void main(String[] args) {
+			int a=100;
+			int b=300;
+			System.out.println(a);
+			System.out.println(b);
+			a=a+3;
+			b=a-300;
+			System.out.println(a);
+			System.out.println(b);
+		}
+	}
