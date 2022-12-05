37e27e71a89a4b006f2b4546e47bdc6b391766f4
Asterisk888 <119795023+Asterisk888@users.noreply.github.com>
2022-12-05 23:22:31 +0900

    Add files via upload

---
+++ hello3.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class hello3 {
+	public static void main(String[] args) {
+		int a=100;
+		int b=300;
+		int tep;
+		System.out.println(a);
+		System.out.println(b);
+		tep=a;
+		a=b;
+		b=tep;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
---
+++ variableTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class variableTest {
+
+	public static void main(String[] args) {
+int a=123232323;
+long t=12321412444L;
+System.out.println(t);
+
+	}
+
+}
