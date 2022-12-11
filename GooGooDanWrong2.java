c8a113b6e8ba6777574db783a7ad49ee43bdd859
Asterisk888 <119795023+Asterisk888@users.noreply.github.com>
2022-12-11 17:55:37 +0900

    Add files via upload

---
+++ GooGooDanWrong2.java
@@ -0,0 +1,15 @@
+package WhileForTest;
+
+public class GooGooDanWrong2 {
+	public static void main(String[] args) {
+		int dan=2;
+		int count=1;
+		for(;dan<=9;dan++);{
+			for(;count<=9;count++);{
+			System.out.println(dan+"X"+count+"="+dan*count);
+		}
+		System.out.println();
+	}
+}
+}
+
